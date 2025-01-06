395b07bce37a5af25b7baaf47826b697bde2a036
Godriele Dela Cruz <godriele11@yahoo.com>
2025-01-06 13:04:29 -0800

    Explained DDoS

---
+++ app.py
@@ -0,0 +1,16 @@
+# DDoS Script
+'''
+ - What it is 
+A distributed denial of service (DDoS) attack is a cybercrime that aims to overload 
+a networkor website with malicious traffic to make it inaccessible or degrade its performance
+
+ - How it works
+Multiple systems flood a target's bandwith or resources with malicious traffic
+
+ - Purpose
+ Prevent Legitimate traffic from reaching its intended destination
+
+ - Imapct
+ Can compromise online security, sales, reputation, and business
+'''
+
